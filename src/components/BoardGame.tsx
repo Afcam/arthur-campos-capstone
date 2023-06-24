adf1efc78d1721d684c6a2847a167a95ec4955e7
AFCampos <afcampos.dev@gmail.com>
2023-06-24 01:13:55 -0400

    feat: add card deck

--- src/components/BoardGame.tsx
+++ src/components/BoardGame.tsx
@@ -16,18 +16,58 @@ import {
   Paper,
   Center,
 } from '@mantine/core';
+import { useState, useEffect } from 'react';
 
+import './BoardGame.scss';
 import DeckCarousel from './DeckCarousel/DeckCarousel';
 import BoardCard from './BoardCard';
+import { IconBrandGit } from '@tabler/icons-react';
+
+const cardDeck = () => {
+  const cards = [];
+  for (let i = 0; i < 20; i++) {
+    cards.push(i);
+  }
+  return cards;
+};
+
+const GitClashCard = () => {
+  return (
+    <Group spacing="0">
+      {cardDeck().map((item, index) => (
+        <Card
+          key={index}
+          w="200px"
+          h="250px"
+          shadow="sm"
+          radius="md"
+          withBorder
+          className="card"
+        >
+          <Stack justify="space-between" h="100%">
+            <IconBrandGit size="100%" />
+
+            <Badge>{index}</Badge>
+          </Stack>
+          {/* <Center mx="auto">
+          </Center> */}
+        </Card>
+      ))}
+    </Group>
+  );
+};
 
 export default function BoardGame() {
+  // const [drawPile, setDrawPile] = useState([]);
+  // const [discardPile, setdDiscardPile] = useState([]);
   return (
     <Stack h={'100%'}>
       <Flex h="100%" gap="md">
         <Paper shadow="xs" withBorder w="100%" h="100%">
-          <Center h="100%" w="100%">
-            Game Board
-          </Center>
+          <Group position="center" p="md" h="100% ">
+            <GitClashCard />
+            {/* <GitClashCard /> */}
+          </Group>
         </Paper>
         <Paper shadow="xs" withBorder w="33%" h="100%">
           Nice
